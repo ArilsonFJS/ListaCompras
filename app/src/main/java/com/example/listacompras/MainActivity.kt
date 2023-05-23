c6bbbcc5783af8097d1a02a053002b4790e8cb31
arilson.fjsilva <arilsonsilva61@gmail.com>
2023-05-22 22:03:11 -0300

    Configuracao do realtimedatabase

--- app/src/main/java/com/example/listacompras/MainActivity.kt
+++ app/src/main/java/com/example/listacompras/MainActivity.kt
@@ -3,20 +3,29 @@ package com.example.listacompras
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.listacompras.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+    lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         tratarLogin()
+
+        binding.fab.setOnClickListener {
+            novoItem()
+        }
     }
 
     fun tratarLogin() {
@@ -28,16 +37,37 @@ class MainActivity : AppCompatActivity() {
                 .setAvailableProviders(providers)
                 .build()
             startActivityForResult(intent, 1)
+        } else {
+            configurarBase()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 1 && resultCode == RESULT_OK){
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             Toast.makeText(this, "Autenticado", Toast.LENGTH_LONG).show()
+            configurarBase()
 
-        }else{
+        } else {
             finishAffinity()
         }
+    }
+
+    fun configurarBase() {
+        FirebaseAuth.getInstance().currentUser?.let {
+            database = FirebaseDatabase.getInstance().reference.child(it.uid)
+        }
+    }
+
+    fun novoItem() {
+        val editText = EditText(this)
+        editText.hint = "Nome do item"
+
+        AlertDialog.Builder(this)
+            .setTitle("Novo item")
+            .setView(editText)
+            .setPositiveButton("Inserir", null)
+            .create()
+            .show()
     }
 }
